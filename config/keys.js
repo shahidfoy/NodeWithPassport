4d904893e3fe32fec9e6d89df057bdc589136d91
shahidfoy <shahidfoy@gmail.com>
2017-12-31 15:36:02 -0700

    updated cookies

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports= {
+    googleClientID: '709262619226-3888c7ehad0msq8r2bbbjb78tshh4ag3.apps.googleusercontent.com',
+    googleClientSecret: 'Ts9c0Unc_Z0rUXcUKJA0Ky8i',
+    facebookClientID: '176551822946333',
+    facebookClientSecret: 'a9a49cf52cb67f43f624630d3fce6173',
+    cookieKey: 'canbearandomstring'
+};
